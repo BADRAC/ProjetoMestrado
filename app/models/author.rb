81939fa57cf8c5a90ced19ffb2c88b2f794daf53
flavi0c0rdula <flavi0c0rdula@gmail.com>
2014-10-08 13:51:26 -0300

    Correção de validação de Autor
    nome e sobrenome

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,6 @@ class Author < ActiveRecord::Base
   has_and_belongs_to_many :articles
 
   validates :first_name, :last_name,
-  			presence: true, 
-  			format: { with: /\A[a-zA-Z]+\z/, message: "Usar apenas letras" }
+  			presence: true
+  			#format: { with: /\A[a-zA-Z]+\z/, message: "Usar apenas letras" }
 end
